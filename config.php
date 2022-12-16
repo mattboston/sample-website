0d41921ba31f40e50b28f618e9dd986200198765
Matt Shields <mattboston@users.noreply.github.com>
2022-12-16 13:49:34 -0500

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_server="{{ salt['pillar.get']('web:config:db_server') }}";
-$db_port="{{ salt['pillar.get']('web:config:db_port') }}";
-$db_username="{{ salt['pillar.get']('web:config:db_usrname') }}";
-$db_password="{{ salt['pillar.get']('web:config:db_password') }}";
